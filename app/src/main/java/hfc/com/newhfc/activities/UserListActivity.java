7b311c5b812307b2c9e2b47f17b1f326ba136741
hp <saxenabhu078@gmail.com>
2018-12-17 10:22:37 +0530

    Commit Dashboard Design

---
+++ app/src/main/java/hfc/com/newhfc/activities/UserListActivity.java
@@ -0,0 +1,26 @@
+package hfc.com.newhfc.activities;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import hfc.com.newhfc.R;
+
+public class UserListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_list);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+
+
+    }
+
+}
