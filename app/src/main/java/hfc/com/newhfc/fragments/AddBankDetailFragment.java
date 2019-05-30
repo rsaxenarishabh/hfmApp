4fd9b693c17a15fefb4c446f5723e1da931e1e7d
hp <saxenabhu078@gmail.com>
2019-05-30 16:57:27 +0530

    Commit Profile Activity Done

--- app/src/main/java/hfc/com/newhfc/fragments/AddBankDetailFragment.java
+++ app/src/main/java/hfc/com/newhfc/fragments/AddBankDetailFragment.java
@@ -5,11 +5,20 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import hfc.com.newhfc.R;
 
 public class AddBankDetailFragment extends Fragment {
 
+
+    private EditText editTextAddhar,editTextPanCard,editTextAccountNumber,editTextIfsc,
+             editTextAccountholderName,editTextBranchName,editTextNominee;
+
+    String aadhar,panCard,accountNumber,ifsc,accountHolder,branchName,nominee;
+    String userId;
+    Button btnAdd;
 
 
     public static AddBankDetailFragment newInstance() {
@@ -26,8 +35,87 @@ public class AddBankDetailFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_bank_detail, container, false);
        // img_profile = view.findViewById(R.id.img_profile);
+        editTextAddhar=view.findViewById(R.id.edittext_aadhar);
+        editTextPanCard=view.findViewById(R.id.edittext_pancard);
+        editTextIfsc=view.findViewById(R.id.edittext_ifsc);
+        editTextAccountholderName=view.findViewById(R.id.edittext_accountholder);
+        editTextNominee=view.findViewById(R.id.edittext_nominee);
+        editTextBranchName=view.findViewById(R.id.edittext_branch);
+        editTextAccountNumber=view.findViewById(R.id.edittext_account);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveDetail();
+            }
+        });
+
+
+
+
 
         return view;
+
+    }
+
+    private void saveDetail() {
+
+        boolean check=true;
+        aadhar=editTextAddhar.getText().toString().trim();
+        panCard=editTextPanCard.getText().toString().trim();
+        accountNumber=editTextAccountNumber.getText().toString().trim();
+        ifsc=editTextIfsc.getText().toString().trim();
+        accountHolder=editTextAccountholderName.getText().toString().trim();
+        branchName=editTextBranchName.getText().toString().trim();
+        nominee=editTextNominee.getText().toString().trim();
+
+
+        if (aadhar.isEmpty()) {
+            editTextAddhar.setError("Field can't be empty");
+            check = false;
+
+        }
+
+        if (panCard.isEmpty()) {
+            editTextPanCard.setError("Field can't be empty");
+            check = false;
+
+        }
+
+        if (accountNumber.isEmpty()) {
+            editTextAccountNumber.setError("Field can't be empty");
+            check = false;
+
+        }
+
+
+        if (ifsc.isEmpty()) {
+            editTextIfsc.setError("Field can't be empty");
+            check = false;
+
+        }
+
+        if (nominee.isEmpty()) {
+            editTextNominee.setError("Field can't be empty");
+            check = false;
+
+        }
+        if (accountHolder.isEmpty()) {
+            editTextAccountholderName.setError("Field can't be empty");
+            check = false;
+
+        }
+        if (branchName.isEmpty()) {
+            editTextBranchName.setError("Field can't be empty");
+            check = false;
+
+        }
+
+        if(check==true)
+        {
+
+
+        }
 
     }
 
