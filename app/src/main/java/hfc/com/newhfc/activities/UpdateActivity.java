77384c8a7608fc6e41fb09b370279cea9ef2496c
Kishan <kishangupta21995@gmail.com>
2019-05-30 19:16:51 -0400

    Profile Commit

--- app/src/main/java/hfc/com/newhfc/activities/UpdateActivity.java
+++ app/src/main/java/hfc/com/newhfc/activities/UpdateActivity.java
@@ -1,16 +1,35 @@
 package hfc.com.newhfc.activities;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import hfc.com.newhfc.R;
 
 public class UpdateActivity extends AppCompatActivity {
+    private EditText name,lastName,phoneNumber,email,dob,address,pinCode;
+    private Button submit;
 
-
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update);
+
+        name=findViewById(R.id.et_firstname);
+        lastName=findViewById(R.id.et_lastname);
+        phoneNumber=findViewById(R.id.et_phonenumber);
+        email=findViewById(R.id.et_email);
+        dob=findViewById(R.id.et_dob);
+        address=findViewById(R.id.et_address);
+        pinCode=findViewById(R.id.et_pincode);
+        submit=findViewById(R.id.btn_submit);
+
+
+
+
+
     }
 }
