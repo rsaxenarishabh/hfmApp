4b6046cd39baf36c30d16c3c3b4f5613c2140cbe
hp <saxenabhu078@gmail.com>
2019-06-05 10:33:10 +0530

    Merge branch 'master' of https://github.com/rsaxenarishabh/hfmApp
    # Conflicts:
    #	.idea/caches/build_file_checksums.ser
    #	app/src/main/java/hfc/com/newhfc/retrofit/ApiInterface.java

---
+++ app/src/main/java/hfc/com/newhfc/activities/DocumentActivity.java
@@ -0,0 +1,33 @@
+package hfc.com.newhfc.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import hfc.com.newhfc.R;
+
+public class DocumentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_document);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        if (item.getItemId() == android.R.id.home)
+        {
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
