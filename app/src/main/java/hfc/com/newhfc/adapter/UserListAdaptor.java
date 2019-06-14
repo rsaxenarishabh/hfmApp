fbbdb9c3452ec01f916c5074a78dc10f1f1131c9
hp <saxenabhu078@gmail.com>
2019-06-14 18:16:06 +0530

    Full Vedio Buy now

--- app/src/main/java/hfc/com/newhfc/adapter/UserListAdaptor.java
+++ app/src/main/java/hfc/com/newhfc/adapter/UserListAdaptor.java
@@ -1,28 +1,32 @@
 package hfc.com.newhfc.adapter;
 
 import android.content.Context;
-import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatDelegate;
+import android.support.v7.widget.AppCompatImageView;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
-import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
 import hfc.com.newhfc.R;
-import hfc.com.newhfc.activities.UserListActivity;
-import hfc.com.newhfc.model.User;
-import hfc.com.newhfc.model.UserList;
 import hfc.com.newhfc.model.userlist.Datum;
 import hfc.com.newhfc.views.CircleImageView;
-
-import static android.view.View.GONE;
 
 /**
  * Created by rbpatel on 3/16/2017.
@@ -75,9 +79,31 @@ public class UserListAdaptor extends RecyclerView.Adapter<UserListAdaptor.ViewHo
             holder.activeImage.setImageResource(R.drawable.ic_blocked);
         }
 
-        Picasso.with(context).load(userlist.getImage())
-                .error(R.drawable.profile_pictures)
-                .into(holder.imageView);
+      /*  if(userlist.getImage()!=null) {
+            Glide.with(context).load(userlist.getImage()).into(holder.simpleImageView);
+        }else {
+            holder.simpleImageView.setImageResource(R.drawable.profile_pictures);
+        }*/
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
+
+        Log.d("Userlist","start");
+       if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.M) {
+           Picasso.with(context).load(userlist.getImage())
+                    .error(R.drawable.profile_pictures)
+                    .into(holder.simpleImageView);
+           holder.simpleImageView.setVisibility(View.VISIBLE);
+           Log.d("Userlist","success");
+           // holder.appCompatImageView.setImageResource(R.drawable.profile_pictures);
+        } else {
+            Picasso.with(context).load(userlist.getImage())
+                    .error(R.drawable.profile_pictures)
+                    .into(holder.imageView);
+           Log.d("Userlist","failed");
+            holder.imageView.setVisibility(View.VISIBLE);
+        }
+
+
+
         if (userlist.getStatus().equalsIgnoreCase("1")) {
             holder.activeImage.setImageResource(R.drawable.ic_activated);
             holder.cardViewList.setOnClickListener(new View.OnClickListener() {
@@ -138,7 +164,10 @@ public class UserListAdaptor extends RecyclerView.Adapter<UserListAdaptor.ViewHo
 
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+        //private CircleImageView imageView;
+        private ImageView simpleImageView;
         private CircleImageView imageView;
+        private AppCompatImageView appCompatImageView;
         private TextView tvName;
         private TextView tvAddress;
         private ImageView activeImage;
@@ -147,6 +176,7 @@ public class UserListAdaptor extends RecyclerView.Adapter<UserListAdaptor.ViewHo
         public ViewHolder(View view) {
             super(view);
             imageView = view.findViewById(R.id.image);
+            simpleImageView = view.findViewById(R.id.simple_image);
             tvName = view.findViewById(R.id.tvName);
             tvAddress = view.findViewById(R.id.tvDesc);
             activeImage = view.findViewById(R.id.tvDelete);
